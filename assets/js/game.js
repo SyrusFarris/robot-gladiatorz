e39fd3746b169ddafec73ea13a4946e74499a9cd
Syrus <syrusfarris@gmail.com>
2022-07-13 22:24:00 -0500

    created a fight function and learned about console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("this is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+// this will do math and log 20
+console.log(10 + 10);
+// What is this?
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
